0f22e0d97768ad71eacf863ddf6bad6341749d51
aushamim <au.shamim0@gmail.com>
2024-08-14 11:46:07 +0600

    [Add] Navbar

--- app/Components/Nav/Nav.jsx
+++ app/Components/Nav/Nav.jsx
@@ -1,25 +1,60 @@
+import Link from "next/link";
 import React from "react";
 
 const Nav = () => {
+  const routes = [
+    {
+      name: "Home",
+      route: "/",
+    },
+    {
+      name: "All Products",
+      route: "/products",
+    },
+    {
+      name: "Profile",
+      route: "/user/profile",
+    },
+    {
+      name: "Admin",
+      route: "/admin",
+    },
+    {
+      name: "Cart",
+      route: "/cart",
+    },
+    {
+      name: "Login",
+      route: "/user/login",
+    },
+  ];
+
   return (
     <nav className="drawer drawer-end">
       <input id="navbar" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content">
         {/* Page content here */}
-        <div className="flex gap-5 bg-red-100 items-center p-3">
-          <div className="text-2xl font-semibold">Alu</div>
+        <div className="flex gap-5 items-center p-3 xl:px-0">
+          <div>
+            <Link href="/" className="text-2xl font-semibold">
+              Alu
+            </Link>
+          </div>
 
           <div className="flex-grow hidden xl:flex justify-end">
-            <a href="/">Home</a>
-            <a href="/">All Products</a>
-            <a href="/">Profile</a>
-            <a href="/">Admin</a>
-            <a href="/">Cart</a>
-            <a href="/">Login</a>
+            {routes?.map((item) => (
+              <Link
+                key={routes?.indexOf(item)}
+                href={item.route}
+                className="hover:text-orange-500 duration-300 ml-3"
+              >
+                {item.name}
+              </Link>
+            ))}
           </div>
 
           <div className="flex-grow flex xl:hidden justify-end">
-            <label htmlFor="navbar" className="">
+            <label htmlFor="navbar">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -44,14 +79,18 @@ const Nav = () => {
           aria-label="close sidebar"
           className="drawer-overlay"
         ></label>
-        <ul className="menu bg-base-200 text-base-content min-h-full w-64 p-4">
+        <ul className="menu bg-base-200 text-base-content min-h-full w-72 p-4">
           {/* Sidebar content here */}
-          <li>
-            <a>Sidebar Item 1</a>
-          </li>
-          <li>
-            <a>Sidebar Item 2</a>
-          </li>
+          {routes?.map((item) => (
+            <li key={routes?.indexOf(item)}>
+              <Link
+                href={item.route}
+                className="hover:text-orange-500 duration-300"
+              >
+                {item.name}
+              </Link>
+            </li>
+          ))}
         </ul>
       </div>
     </nav>
